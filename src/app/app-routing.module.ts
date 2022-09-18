bd46d363eccf66910875db84e0c663ceff67abcc
NiteshKumar41 <niteshjangir41@gmail.com>
2022-09-18 07:13:41 +0530

    intial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './componenets/dashboard/dashboard/dashboard.component';
+import { LayoutComponent } from './componenets/dashboard/layout/layout.component';
+import { LoginComponent } from './componenets/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'login', component:LoginComponent},
+  {path:'', redirectTo:'dashboard',pathMatch:'full'},
+  {
+    path:'', component:LayoutComponent,
+    children:[
+      {
+        path:'',loadChildren: () =>import('./componenets/dashboard/layout.module').then(m=>m.LayoutModule)
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
